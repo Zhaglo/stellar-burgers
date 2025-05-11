e90cc66c02d2a3609b57e4d1db569ea071b35851
Zhaglo <jagloig2005@gmail.com>
2025-05-11 14:07:08 +0300

    feat: routes in app, protected-route, userInfoSlice

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,59 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed, ForgotPassword, Login, NotFound404, Profile, ProfileOrders, Register, ResetPassword } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from '../../services/store';
+import { useEffect } from 'react';
+import { checkUserAuth } from 'src/services/slices/UserInfoSlice';
+import { ProtectedRoute } from '../protected-route';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const locationState = location.state as { background?: Location };
+  const background = locationState && location.state?.background;
+
+  const closeModal = () => {
+    navigate(-1);
+  }
+
+  useEffect(() => {
+    dispatch(checkUserAuth());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+
+        <Route path='/login' element={<ProtectedRoute onlyUnAuth><Login /></ProtectedRoute>} />
+        <Route path='/register' element={<ProtectedRoute onlyUnAuth><Register /></ProtectedRoute>} />
+        <Route path='/forgot-password' element={<ProtectedRoute onlyUnAuth><ForgotPassword /></ProtectedRoute>} />
+        <Route path='reset-password' element={<ProtectedRoute><ResetPassword /></ProtectedRoute>} />
+
+        <Route path='profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path='/profile/orders' element={<ProtectedRoute><ProfileOrders /></ProtectedRoute>} />
+        <Route path='/profile/orders/:number' element={<ProtectedRoute><OrderInfo /></ProtectedRoute>}/>
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      { background && (
+        <Routes>
+          <Route path='feed/:number' element={<Modal title={''} onClose={closeModal}><OrderInfo /></Modal>} />
+          <Route path='/ingredients/:id' element={<Modal title={'Детали ингредиента'} onClose={closeModal}><IngredientDetails /></Modal>} />
+          <Route path='/profile/orders/:number' element={<ProtectedRoute><Modal title={''} onClose={closeModal}><OrderInfo /></Modal></ProtectedRoute>}/>
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,19 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import userStateSlice from './slices/UserInfoSlice';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = combineReducers({
+  [userStateSlice.name]: userStateSlice.reducer,
+});
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
